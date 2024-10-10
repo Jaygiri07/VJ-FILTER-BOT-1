e0eab8451dc5025a30530dcafee074ef796583cd
Fmmbot09 <j32025026@gmail.com>
2024-10-10 21:27:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -111,9 +111,9 @@ Cᴏᴍᴍᴀɴᴅs Aɴᴅ Usᴀɢᴇ:
     EXTRAMOD_TXT = """ʜᴇʟᴘ: Exᴛʀᴀ Mᴏᴅᴜʟᴇs
 <b>ɴᴏᴛᴇ:</b>
 my features Stay here new features coming soon...  
- <b>✯ Maintained by : <a href=https://t.me/rishu1286>☢Rishu☢</a></b>
+ <b>✯ Maintained by : <a href=https://t.me/Stylish_Star2>☢Rebel☢</a></b>
   
- <b>✯ Join here : <a href=https://t.me/ur_rishu_143>☢Join my updateds☢</a></b> 
+ <b>✯ Join here : <a href=https://t.me/Filmone_Backup>☢Join my updateds☢</a></b> 
   
  ./id - <code>ɢᴇᴛ ɪᴅ ᴏꜰ ᴀ ꜱᴘᴇᴄɪꜰɪᴇᴅ ᴜꜱᴇʀ.</ 
  code> 
